5ec887064cfb2128d3db547b045e0a8ff2b4a5b7
uditsharma348 <uditsharma348@gmail.com>
2023-01-21 17:30:33 +0530

    cal

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname,"./public")))
+
+app.get("/",(req,res)=>{
+    res.sendFile("index")
+})
+app.listen(3000,()=>{
+    console.log("server run")
+})
